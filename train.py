22d1a1f5b50108ff7586eb43624181e23f4b5fa2
mohamed tarek <132160918+motarek122112@users.noreply.github.com>
2026-04-26 04:35:10 +0300

    Update train.py

--- train.py
+++ train.py
@@ -1,19 +1,8 @@
 import mlflow
 
-mlflow.set_experiment("mlops-quiz")
+acc = 0.90
 
 with mlflow.start_run() as run:
-    accuracy = 0.90
-
-    mlflow.log_metric("accuracy", accuracy)
-
-    run_id = run.info.run_id
-
-    with open("model_info.txt", "w") as f:
-        f.write(run_id)
-
-    with open("accuracy.txt", "w") as f:
-        f.write(str(accuracy))
-
-    print(f"Run ID: {run_id}")
-    print(f"Accuracy: {accuracy}")
+    mlflow.log_metric("accuracy", acc)
+    open("model_info.txt", "w").write(run.info.run_id)
+    open("accuracy.txt", "w").write(str(acc))
